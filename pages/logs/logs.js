6e67edba0dd3246d4a2d4e7abfb5f858d5ea0698
unknown <zhanweichao@utry.msc>
2018-02-07 15:31:18 +0800

    wxApp

---
+++ pages/logs/logs.js
@@ -0,0 +1,18 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  },
+  tapLogs: function(e) {
+    console.log(e.touches, e.changedTouches)
+  }
+})

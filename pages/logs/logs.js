83f3b9311b1e814e564208e0d712007031d7c67d
unknown <zhanweichao@utry.msc>
2018-02-07 15:53:13 +0800

    position

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -13,6 +13,14 @@ Page({
     })
   },
   tapLogs: function(e) {
-    console.log(e.touches, e.changedTouches)
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        var latitude = res.latitude // 经度
+        var longitude = res.longitude // 纬度
+
+        console.log(latitude, longitude)        
+      }
+    })
   }
 })

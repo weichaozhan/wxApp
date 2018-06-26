0b5fa7a86e6ddc7af795f861a934ffbc01b742a2
zhanweichao <921391554@qq.com>
2018-06-26 14:10:20 +0800

    扫码

--- pages/start/start.js
+++ pages/start/start.js
@@ -20,5 +20,24 @@ Page({
         })
       }
     })
+  },
+  useCamera(e) {
+    wx.scanCode({
+      success: function(data) {
+        wx.showModal({
+          title: '提示',
+          content: JSON.stringify(data),
+          success: function (res) {
+            if (res.confirm) {
+              wx.showToast({
+                title: '用户点击确定'
+              })
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
+        })
+      }
+    })
   }
 })

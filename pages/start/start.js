2b5e363b347a8dce83b5f13505e2656f3e324f9b
zhanweichao <921391554@qq.com>
2018-06-27 11:18:09 +0800

    request

--- pages/start/start.js
+++ pages/start/start.js
@@ -22,6 +22,13 @@ Page({
     })
   },
   useCamera(e) {
+    wx.request({
+      url: 'https://cn.bing.com/search?q=test&qs=n&form=QBLH&sp=-1&pq=test&sc=8-4&sk=&cvid=D58AEF64EDB546ED823709BB423A9F9D',
+      method: 'get',
+      success: (res) => {
+        console.log(res)
+      }
+    })
     wx.scanCode({
       success: function(data) {
         wx.showModal({
